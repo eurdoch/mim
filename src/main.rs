a08882b88ee92a752f063128c36bb969cd1d5182
George Balch <gbalch714@gmail.com>
2025-01-18 21:43:33 -0800

    Basic command line program that echos arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(name = "mim")]
+#[command(about = "Prints all command line arguments")]
+struct Cli {
+    /// The words to be printed
+    #[arg(required = true, num_args = 1..)]
+    words: Vec<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    
+    // Print all words passed as arguments
+    for word in &cli.words {
+        println!("{}", word);
+    }
 }
